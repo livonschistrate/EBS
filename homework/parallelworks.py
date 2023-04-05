3afbc8560029882901636c2e34093404646ebe12
Liviu Istrate <75808975+livonschistrate@users.noreply.github.com>
2023-04-05 15:20:57 +0300

    added readme + cases for the threading feat

--- homework/parallelworks.py
+++ homework/parallelworks.py
@@ -5,7 +5,7 @@ from subscribe import generateSubs
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="parallelworks.py does parallel running")
     parser.add_argument('--threads', '-t', default=1, type=int, help="Number of workers/threads to execute the operations")
-    parser.add_argument('--settype', '-s', default='p', type=str, help="Type of set to choose: publications (p) or subscriptions (s)")
+    parser.add_argument('--settype', '-s', default='p', type=str, help="Type of set to choose: publications (p) or subscriptions (s) or both (b)")
     parser.add_argument('--number', '-n', default=100, type=int, help="Number of pubs/subs to generate")
     args = parser.parse_args()
     
@@ -15,21 +15,50 @@ if __name__ == '__main__':
     } 
     threads, totaltime = [], 0
     tp , ts = 0, 0
-    for i in range(args.threads):
-        chosen_operation = random.choice(list(opertype.keys()))
-        if chosen_operation == 'p':
-            tp += 1 
-        else:
-            ts += 1
-        t = threading.Thread(target=opertype[chosen_operation], args=(args.number,))
+    
+    # case 1 : one thread generates pubs/subs
+    if args.threads == 1 and args.settype in ['p', 's']:
+        t = threading.Thread(target=opertype[args.settype], args=(args.number,))
         threads.append(t)
         start = time.time()
         t.start()
         t.join()
         end = time.time()
-        totaltime += end - start
-    print(tp, "threads generated publications")
-    print(ts, "threads genereated subscriptions")
-    print("We ruled {0} threads of {1} {2}, the execution time is: {3}".format(args.threads, args.number, 
+        print("One thread generated {0} {1} in an execution time of {2}".format(args.number,
+                                                    "subscriptions" if args.settype == 's' else "publications",
+                                                    end-start))
+        
+    # case 2 : multiple threads generates both pubs/subs
+    elif args.threads > 1 and args.settype == 'b':
+        for i in range(args.threads):
+            chosen_operation = random.choice(list(opertype.keys()))
+            if chosen_operation == 'p':
+                tp += 1 
+            else:
+                ts += 1
+            t = threading.Thread(target=opertype[chosen_operation], args=(args.number,))
+            threads.append(t)
+            start = time.time()
+            t.start()
+            t.join()
+            end = time.time()
+            totaltime += end - start
+        print(tp, "threads generated publications")
+        print(ts, "threads genereated subscriptions")
+        print("We ruled {0} threads of {1} {2}, the execution time is: {3}".format(args.threads, args.number, 
                                                                                "messages",
+                                                                               totaltime))
+    
+    # case 3: multiple threads generate only publications/ only subscriptions
+    elif args.settype in ['p', 's']:
+        for i in range(args.threads):
+            t = threading.Thread(target=opertype[args.settype], args=(args.number,))
+            threads.append(t)
+            start = time.time()
+            t.start()
+            t.join()
+            end = time.time()
+            totaltime += end - start
+        print("We ruled {0} threads of {1} {2}, the execution time is: {3}".format(args.threads, args.number, 
+                                                                               "subscriptions" if args.settype == 's' else "publications",
                                                                                totaltime))
